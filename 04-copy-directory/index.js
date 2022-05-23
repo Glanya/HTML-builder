4f7f59b6fc8ff968bbb7f35c704da4c45db91f32
Glanya <sokolovskayaglafira@gmail.com>
2022-05-23 19:41:45 +0300

    feat: complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const folder = path.join(__dirname, 'files');
+const folderCopy = path.join(__dirname, 'files-copy');
+
+async function copyDir() {
+  try {
+    await fs.rm(folderCopy, { recursive: true, force: true });
+    await fs.mkdir(folderCopy, { recursive: true });
+
+    const files = await fs.readdir(folder, { withFileTypes: true });
+    files.filter(file => file.isFile());
+    files.forEach(file => {
+      fs.copyFile(path.join(folder, file.name), path.join(folderCopy, file.name));
+    });
+  } catch (error) {
+    console.log('Error: ', error);
+  }
+}
+copyDir();
+
+
+
+
 
